a7cac78d3ff5ca151073099898ec4cea0f14c024
Siyue Zhang <syzhang@thoughtworks.com>
2016-11-17 14:11:12 +0800

    should wait command after sell command

---
+++ source/src/command/SellCommand.js
@@ -0,0 +1,15 @@
+import Command from "./Command";
+import * as Status from '../status';
+
+export default class SellCommand extends Command{
+    constructor(map, position){
+        super();
+        this.map = map;
+        this.position = position;
+    }
+
+    execute(player){
+        player.sellEstate(this.map, this.position);
+        return Status.WAIT_COMMAND;
+    }
+}
---
+++ source/test/command/sell-command-spec.js
@@ -0,0 +1,26 @@
+import Player from '../../src/Player';
+import Estate from '../../src/place/Estate';
+import Map from '../../src/Map';
+import SellCommand from '../../src/command/SellCommand';
+import * as Status from '../../src/status';
+
+describe('sell command test', () => {
+    it('should be wait command after sell command', () => {
+        let player = new Player();
+        let estate = new Estate(200);
+        let map = new Map(estate);
+        estate.owner = player;
+        estate.level = 1;
+        player.estates = [estate];
+        player.balance = 0;
+        let sellCommand = new SellCommand(map, 0);
+
+        player.execute(sellCommand);
+
+        expect(player.status).toBe(Status.WAIT_COMMAND);
+        expect(player.balance).toBe(2 * 2 * 200);
+        expect(player.estates.length).toBe(0);
+        expect(estate.owner).toBe(null);
+        expect(estate.level).toBe(0);
+    });
+})
