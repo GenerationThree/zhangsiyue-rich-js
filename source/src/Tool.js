7909d9db406b15de5463bc8ef71563a5f0b248c7
Siyue Zhang <syzhang@thoughtworks.com>
2016-11-17 16:12:50 +0800

    tool take affect

--- source/src/Tool.js
+++ source/src/Tool.js
@@ -10,4 +10,41 @@ export default class Tool{
         this.type = type;
         this.price = TOOL_TYPE[this.type];
     }
+
+    use(map, currentPlace, distance){
+        let currentIndex = map.places.indexOf(currentPlace);
+        let position = distance + currentIndex;
+
+        let length = map.places.length;
+
+        if (position < 0)
+            position = length - (0 - position) % length;
+
+        else if(position > 0)
+            position = position % length;
+
+        let target = map.places[position];
+
+        switch (this.type) {
+            case 1:
+                if(target.blocked === false && target.hasBomb === false && target.locateHere.length == 0) {
+                    target.blocked = true;
+                    return true;
+                }
+                return false;
+            case 2:
+                for(let i = 1; i <= distance; i++) {
+                    map.places[currentIndex + i].blocked = false;
+                    map.places[currentIndex + i].hasBomb = false;
+                }
+                return true;
+            case 3:
+                if(target.blocked === false && target.hasBomb === false && target.locateHere.length == 0) {
+                    target.hasBomb = true;
+                    return true;
+                }
+            default:
+                return false;
+        }
+    }
 }
---
+++ source/test/tool-spec.js
@@ -0,0 +1,79 @@
+import Place from '../src/place/Place';
+import Map from '../src/Map';
+import Tool from '../src/Tool';
+import Player from "../src/Player";
+
+
+describe('tool test', () => {
+    let map;
+    let startPoint;
+    let targetPoint;
+    let otherPoint;
+    let player;
+    let tool;
+
+    beforeEach(() =>{
+        startPoint = new Place();
+        targetPoint = new Place();
+        otherPoint = new Place();
+        map = new Map(startPoint, otherPoint, targetPoint);
+        player = new Player();
+    });
+
+    it('should use block at specified place', () => {
+        tool = new Tool(1);
+
+        expect(targetPoint.blocked).toBe(false);
+
+        expect(tool.use(map, startPoint, 2)).toBe(true);
+
+        expect(targetPoint.blocked).toBe(true);
+    });
+
+    it('should use bomb at specified place', () => {
+        tool = new Tool(3);
+
+        expect(targetPoint.blocked).toBe(false);
+
+        expect(tool.use(map, startPoint, 2)).toBe(true);
+
+        expect(targetPoint.hasBomb).toBe(true);
+    });
+
+    it('should remove tools in distance when use robot', () => {
+        tool = new Tool(2);
+
+        let bombPoint = new Place();
+        bombPoint.hasBomb = true;
+
+        let blockPoint = new Place();
+        blockPoint.blocked = true;
+
+        let outPoint = new Place();
+        outPoint.hasBomb = true;
+
+        map = new Map(startPoint, bombPoint, blockPoint, outPoint);
+
+        expect(tool.use(map, startPoint, 2)).toBe(true);
+
+        expect(bombPoint.hasBomb).toBe(false);
+        expect(blockPoint.blocked).toBe(false);
+        expect(outPoint.hasBomb).toBe(true);
+    });
+
+    it('should not set tool where already have tool', () => {
+        tool = new Tool(1);
+        targetPoint.hasBomb = true;
+
+        expect(tool.use(map, startPoint, -1)).toBe(false);
+    });
+
+    it('should not set tool where player locate', () => {
+        tool = new Tool(1);
+        targetPoint.hasBomb = false;
+        targetPoint.locateHere.push(player);
+
+        expect(tool.use(map, startPoint, -1)).toBe(false);
+    });
+
+});
